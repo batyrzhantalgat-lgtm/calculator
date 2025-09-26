bba1d8fc81aa0bc10928db4a22650a907f733fce
batyrzhantalgat-lgtm <batyrzhantalgat@gmail.com>
2025-09-26 10:29:01 +0500

    Add files via upload

---
+++ work.java
@@ -0,0 +1,105 @@
+package home;
+import java.util.Scanner;
+
+public class work {
+    public static void main(String[] args) {
+        //3.1
+        /*Scanner scan = new Scanner(System.in);
+        System.out.print("Введите a: ");
+        double a = scan.nextDouble();
+
+
+        System.out.print("Введите b: ");
+        double b = scan.nextDouble();
+
+       System.out.print("Введите c: ");
+        double c = scan.nextDouble();
+
+       double sqr = Math.pow(b,2);
+        double sum = 4*a*c;
+        double roots = sqr-sum;
+
+        if(roots == 0) {
+              double d = -sqr/2*a;
+              System.out.print(d);
+        }
+        //   else if(roots >= 1) {
+          //  double e = Math.sqrt(roots);
+            //double f = (-b+e)/-2*a;
+           // System.out.print(f);
+       // }
+        //else
+        //{
+          //  System.out.print("Нет ответа");
+        //}
+
+
+*/
+
+    //3.2
+   /* Scanner scan = new Scanner(System.in);
+    int a = (int)(Math.random() * 10);
+   int b = 9-a;
+    System.out.println("a ="+ a);
+    System.out.println("b ="+ b);
+    System.out.println("Введите сумму этих чисел");
+      double c = scan.nextDouble();
+        if(c == 9){
+            System.out.print("wriht");
+        }
+         else {
+             System.out.print("wrong");
+       }
+
+*/
+        //3.4
+       /* Scanner scan = new Scanner(System.in);
+       int a = (int)(Math.random() * 13);
+        if(a == 1) {
+            System.out.println("January");
+        }
+        else if(a == 2) {
+            System.out.println("February");
+        }
+       else if(a == 3) {
+            System.out.println("march");
+        }
+        else if(a == 4) {
+            System.out.println("april");
+        }
+        else if(a == 5) {
+            System.out.println("may");
+        }
+        else if(a == 6) {
+            System.out.println("June");
+        }
+        else if(a == 7) {
+            System.out.println("July");
+        }
+        else if(a == 8) {
+            System.out.println("august");
+        }
+        else if(a == 9) {
+            System.out.println("September");
+        }
+        else if(a == 10) {
+            System.out.println("october");
+        }
+        else if(a == 11) {
+            System.out.println("november");
+        }
+        else{
+            System.out.println("december");
+        }
+
+
+
+
+
+
+
+
+
+*/
+   }
+}
